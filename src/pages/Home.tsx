03251cd14c8e2e0ff736d6f84935c541d6853cc9
Kate Johnson <johnson.kat.g@gmail.com>
2022-05-30 03:04:06 -0400

    added error handling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,68 +1,86 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { FC, useState } from 'react';
 import { Book } from '../components/Book';
 
 export interface Ibook {
-  id: string,
-  title: string,
-  authors: string[],
-  publishingCompany?: string
+  id: string;
+  title: string;
+  authors: string[];
+  publishingCompany?: string;
 }
 
 interface IbookData {
-  id: string
+  id: string;
   volumeInfo: {
-    title: string,
-    categories?: string[],
-    publisher?: string,
-    authors: string[],
-    description: string,
-    infoLink: string,
+    title: string;
+    categories?: string[];
+    publisher?: string;
+    authors: string[];
+    description: string;
+    infoLink: string;
     imageLinks?: {
-      thumbnail: string,
+      thumbnail: string;
     };
-    publishedDate: string,
+    publishedDate: string;
   };
 }
 
 export const Home: FC = () => {
   const [query, setQuery] = useState<string>('');
-  const [books, setBooks] = useState<Ibook[]>([])
+  const [books, setBooks] = useState<Ibook[]>([]);
+  const [error, setError] = useState<string>('');
 
-  const handleSearch = async (e: React.FormEvent<HTMLFormElement> | React.MouseEvent) => {
+  const handleSearch = async (
+    e: React.FormEvent<HTMLFormElement> | React.MouseEvent
+  ) => {
     e.preventDefault();
     if (!query) {
-      alert('please enter a search term');
+      setError('please enter a search term');
+      setTimeout(() => {
+        setError('');
+      }, 3000);
     }
     try {
       const { data } = await axios.get(
         `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=5`
       );
       if (!data) {
-        alert('please enter a different search term');
+        setError('please enter a different search term');
+        setTimeout(() => {
+          setError('');
+        }, 3000);
         setQuery('');
       }
       const bookList: Ibook[] = [];
       data.items.map((item: IbookData) => {
-        let bookObj:Ibook = {
+        let bookObj: Ibook = {
           id: item.id,
           title: item.volumeInfo.title,
           authors: item.volumeInfo.authors,
           publishingCompany: item.volumeInfo.publisher
-        }
+        };
         bookList.push(bookObj);
-      })
+      });
       setBooks(bookList);
       setQuery('');
     } catch (err) {
-      alert('something went wrong, please try another search')
+      if (err instanceof Error) {
+        console.log(`Error: ${err.message}`);
+        console.log(err)
+      }
+      if (err instanceof AxiosError && err.response?.status != 400) {
+        setError('something went wrong, please try again');
+        setTimeout(() => {
+          setError('');
+        }, 3000);
+      }
       setQuery('');
-      console.log(err);
     }
   };
 
   return (
     <div>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSearch}>
         <input
           name="search"
@@ -75,14 +93,16 @@ export const Home: FC = () => {
       <button type="button" onClick={handleSearch}>
         Search
       </button>
-      {books && books.map((book, i) => (
-        <Book
-          id={book.id}
-          title={book.title}
-          authors={book.authors}
-          publishingCompany={book.publishingCompany}
-          key={i} />
-      ))}
+      {books &&
+        books.map((book, i) => (
+          <Book
+            id={book.id}
+            title={book.title}
+            authors={book.authors}
+            publishingCompany={book.publishingCompany}
+            key={i}
+          />
+        ))}
     </div>
   );
 };
